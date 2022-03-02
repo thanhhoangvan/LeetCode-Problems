1bbc9f33707ac54384c1c2c6ab57672e180a8373
Thanh HoangVan <thanh.hoangvan051199@gmail.com>
2022-03-03 01:49:44 +0700

    add file code for slove problem 2

--- [1]_Two_Sum/TwoSum.py
+++ [1]_Two_Sum/TwoSum.py
@@ -11,7 +11,7 @@ Example:
 """
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums:lList[int], target: int) -> List[int]:
         for i in range(len(nums) - 1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
